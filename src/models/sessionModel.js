28e75a7bc9f39ef07d5877acadd26f7c2b82b5f3
USMPDNBJ <david_nolasco@usmp.pe>
2025-05-24 00:31:29 -0500

    ajuste session

--- src/models/sessionModel.js
+++ src/models/sessionModel.js
@@ -34,25 +34,25 @@ export const getSessionByUserIdService = async (id) => {
 };
 
 // Crear una nueva sesión
-export const createSessionService = async (usuario_id, profesional_id, fecha, hora_inicio, hora_fin, estado, evaluacion, enlace) => {
+export const createSessionService = async (usuario_id, profesional_id, id_pago, fecha, hora_inicio, hora_fin, estado, evaluacion, enlace) => {
     const result = await pool.query(
-        `INSERT INTO t_session (usuario_id, profesional_id, fecha, hora_inicio, hora_fin, estado, evaluacion, enlace)
-         VALUES ($1, $2, $3, $4, $5, $6, $7, $8)
+        `INSERT INTO t_session (usuario_id, profesional_id, id_pago, fecha, hora_inicio, hora_fin, estado, evaluacion, enlace)
+         VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)
          RETURNING *`,
-        [usuario_id, profesional_id, fecha, hora_inicio, hora_fin, estado, evaluacion, enlace]
+        [usuario_id, profesional_id, id_pago, fecha, hora_inicio, hora_fin, estado, evaluacion, enlace]
     );
     return result.rows[0];
 };
 
 // Actualizar una sesión existente
-export const updateSessionService = async (usuario_id, profesional_id, fecha, hora_inicio, hora_fin, estado, evaluacion, enlace, id) => {
+export const updateSessionService = async (usuario_id, profesional_id, id_pago, fecha, hora_inicio, hora_fin, estado, evaluacion, enlace, id) => {
     const result = await pool.query(
         `UPDATE t_session 
-         SET usuario_id = $1, profesional_id = $2, fecha = $3, hora_inicio = $4, hora_fin = $5,
-             estado = $6, evaluacion = $7, enlace = $8
-         WHERE id = $9
+         SET usuario_id = $1, profesional_id = $2, id_pago=$3, fecha = $4, hora_inicio = $5, hora_fin = $6,
+             estado = $7, evaluacion = $8, enlace = $9
+         WHERE id = $10
          RETURNING *`,
-        [usuario_id, profesional_id, fecha, hora_inicio, hora_fin, estado, evaluacion, enlace, id]
+        [usuario_id, profesional_id, id_pago, fecha, hora_inicio, hora_fin, estado, evaluacion, enlace, id]
     );
     return result.rows[0];
 };
